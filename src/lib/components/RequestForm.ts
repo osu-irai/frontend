6a9371493844c0a37d703fbd716a3eb3c25bc53f
Chiffa <artemy040404@gmail.com>
2025-06-14 20:14:21 +0300

    fix: Cleanup form stuff

--- src/lib/components/RequestForm.ts
+++ src/lib/components/RequestForm.ts
@@ -1,16 +1,29 @@
-import { err, ok, type Result } from "neverthrow";
+import { err, ok, Result } from "neverthrow";
 import { ParseError } from "../../app";
+
+function fetchPlayer(form: FormData): Result<string, ParseError> {
+  const playerEntry = form.get("destination");
+  return (playerEntry === null)
+    ? err(ParseError.PlayerFetchError)
+    : ok(playerEntry.toString());
+}
+
+function fetchBeatmap(form: FormData): Result<string, ParseError> {
+  const beatmapEntry = form.get("beatmap");
+  return (beatmapEntry === null)
+    ? err(ParseError.BeatmapFetchError)
+    : ok(beatmapEntry.toString());
+}
 
 const beatmapRegex: RegExp = RegExp(
   /(?:https:\/\/osu\.ppy\.sh\/beatmapsets\/\d+#[a-z]*\/)?(\d+)(?:\/.*)?/,
 );
 export function parseBeatmapId(beatmap: string): Result<BeatmapId, ParseError> {
   const capture = beatmapRegex.exec(beatmap);
-  const beatmapId = capture?.[1] ?? "";
-  if (beatmapId === undefined) {
-    return err(ParseError.BeatmapParseError);
-  }
-  return ok(parseInt(beatmapId) as BeatmapId);
+  const beatmapId = capture?.[1];
+  return (beatmapId === undefined)
+    ? err(ParseError.BeatmapParseError)
+    : ok(parseInt(beatmapId) as BeatmapId);
 }
 
 const playerRegex: RegExp = RegExp(
@@ -19,10 +32,9 @@ const playerRegex: RegExp = RegExp(
 export function parsePlayerId(player: string): Result<PlayerId, ParseError> {
   const capture = playerRegex.exec(player);
   const playerId = capture?.[1];
-  if (playerId === undefined) {
-    return err(ParseError.PlayerParseError);
-  }
-  return ok(parseInt(playerId) as PlayerId);
+  return (playerId === undefined)
+    ? err(ParseError.PlayerParseError)
+    : ok(parseInt(playerId) as PlayerId);
 }
 
 export type BeatmapId = number & { readonly __tag: unique symbol };
@@ -30,22 +42,15 @@ export type PlayerId = number & { readonly __tag: unique symbol };
 
 export function parseFormData(
   form: FormData,
-): Result<[PlayerId, BeatmapId], string> {
-  const player = form.get("destination")?.toString();
-  if (player === undefined) {
-    return err("Failed to fetch player");
-  }
-  const beatmap = form.get("beatmap")?.toString();
-  if (beatmap === undefined) {
-    return err("failed to fetch beatmap");
-  }
-  const playerId = parsePlayerId(player);
+): Result<[PlayerId, BeatmapId], ParseError> {
+  const playerId = fetchPlayer(form).andThen(parsePlayerId);
+  const beatmapId = fetchBeatmap(form).andThen(parseBeatmapId);
+
   if (playerId.isErr()) {
-    return err("Failed to parse player");
+    return err(playerId.error);
   }
-  const beatmapId = parseBeatmapId(beatmap);
   if (beatmapId.isErr()) {
-    return err("Failed to parse beatmap");
+    return err(beatmapId.error);
   }
   return ok([
     playerId.value,
